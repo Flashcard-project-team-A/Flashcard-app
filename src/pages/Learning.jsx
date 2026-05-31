9c3e51cc095223b6f0a6edc111b98b11fb1d132d
VelvetCommit <kristin20.04sarkoian@gmail.com>
2026-05-31 02:30:23 +0200

    Delete und Edit Buttons eingefügt und Lösch-Popup Fenster hinzugefügt

--- src/pages/Learning.jsx
+++ src/pages/Learning.jsx
@@ -14,12 +14,36 @@ export default function Learning(){
 //Klick auf Bestätigungsbutton soll Fortschrittsanzeige erhöhen
     const [progress, setProgress] = useState(0);
 
+    //Für das Popup-Fenster, am Anfang nicht sichtbar-> false
+    const [showPopup, setShowPopup] = useState(false);
+
     function handleClick(){
         setProgress(progress +  1);
     }
     
+//Bestätigung im Pop-up-Fenster fürs Löschen
+    function handleDeleteCard() {
+        setShowPopup(true);
+    }
+
+    function handleCancelDelete() {
+        setShowPopup(false);
+    }
+
+    function handleConfirmDelete() {
+
+        setShowPopup(false);
+    }
+
+
     return (
     <>
+{/* Steuerungsbuttons oben rechts*/}
+        <div className="top-buttons">
+            <button className="edit-button">✏️Bearbeiten</button>
+            <button className="delete-button" onClick={handleDeleteCard}>🗑️Löschen</button>
+        </div>
+
 {/*Name des aktuellen Lernsets*/}
         <h2 style={{color:"white"}}>Name des Sets</h2> 
 
@@ -37,6 +61,24 @@ export default function Learning(){
             <LearnedBtn onClick={handleClick}/>
             <TryAgainBtn/>
         </div>
+
+
+{/*Pop-up-Fenster bauen */}
+        {showPopup && (
+            <div className="popup-background">
+                <div className="popup-box">
+                    <h3>Bist du sicher?</h3>
+                    <p>Möchtest du diese Lernkarte wirklich löschen?</p>
+                    <div className="popup-buttons">
+                        <button className="popup-cancel" onClick={handleCancelDelete}>Abbrechen</button>
+                        <button className="popup-confirm" onClick={handleConfirmDelete}>Löschen</button>
+                    </div>
+                </div>
+            </div>
+        )}
+
+
+
     </>
     );
 }
