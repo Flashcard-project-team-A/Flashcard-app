740676cb7e5a57cb9fa70f413086907cd674a0cb
VelvetCommit <kristin20.04sarkoian@gmail.com>
2026-06-08 15:12:26 +0200

    Bearbeitungsfunktion für Lernkarten per Popup-Fenster hinzugefügt, Logik der Löschfunktion überarbeitet und css optimiert

--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -1,3 +1,53 @@
-export default function Edit(){
-    
+import {useState} from "react";
+
+export default function Edit({card, onClose, onSave}){
+
+    //falls karte leer-> nimm leeren Text ""
+    const [frage, setFrage]= useState(card?.frage || "");
+    const [antwort, setAntwort]= useState(card?.antwort || ""); 
+
+    //prüfen ob Felder leer sind
+    function manageSave(){
+        if(frage.trim()===""|| antwort.trim()===""){ //leerzeichen wegschneidrn
+            alert("Fehler: Eingabefelder dürfen nicht leer sein!");
+            return;
+        }onSave(frage, antwort);
+    }
+
+    return (
+        //Popup-fesnter style wiederverwenden
+        <div className="popup-background">
+            <div className="popup-box">
+                <h3>Aktuelle Karte bearbeiten</h3>
+                
+                {/*Eingabefeld für  frage */}
+                <div style={{ marginTop: "1.25rem", textAlign: "left" }}>
+                    <label style={{ color: "white", display: "block", marginBottom: "0.625rem", fontSize: "1.25rem" }}>Frage:</label>
+                    <input 
+                        type="text" 
+                        value={frage} 
+                        onChange={(e) => setFrage(e.target.value)} 
+                        style={{ width: "95%", padding: "0.625rem", borderRadius: "5px",border: "1px solid #ffffff",fontSize: "0.85rem"}}
+                    />
+                </div>
+
+                {/*Eingabefeld für Antwort*/}
+                <div style={{ marginTop: "1.25rem", textAlign: "left" }}>
+                    <label style={{ color: "white", display: "block", marginBottom: "0.625rem", fontSize: "1.25rem" }}>Antwort:</label>
+                    <input 
+                        type="text" 
+                        value={antwort} 
+                        onChange={(e) => setAntwort(e.target.value)} 
+                        style={{ width: "95%", padding: "0.625rem",borderRadius: "5px", border: "1px solid #ffffff",fontSize: "0.85rem"}}
+                    />
+                </div>
+
+                {/*Die beiden buttons unten */}
+                <div className="popup-buttons">
+                    <button className="popup-cancel" onClick={onClose}>Abbrechen</button>
+                    <button className="popup-update-confirm" onClick={manageSave} >Speichern</button>
+                </div>
+            </div>
+        </div>
+    );
 }
--- src/pages/Learning.jsx
+++ src/pages/Learning.jsx
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react';
-import {useParams, useNavigate} from "react-router-dom";
+import {useParams} from "react-router-dom";
 
 import "../components/Temp.css";
 import {db} from "../db";
@@ -9,6 +9,7 @@ import TryAgainBtn from "../components/TryAgainBtn.jsx";
 import Progressbar from "../components/Progressbar.jsx";
 import AnswerCard from "../components/AnswerCard.jsx";
 import QuestionCard from "../components/QuestionCard.jsx";
+import Edit from "../components/Edit.jsx";
 
 
 export default function Learning(){
@@ -18,13 +19,13 @@ export default function Learning(){
     //Set laden
 //id aus URL holen (z.B. .../learning/1 -> id = 1)
     const {id} = useParams(); 
-    const navigate = useNavigate();
 
     const [progress, setProgress] = useState(0);
     const [currentSet, setCurrentSet] = useState(null);
     const [index, setIndex] = useState(0);
     //Für das Popup-Fenster, am Anfang nicht sichtbar-> false
     const [showPopup, setShowPopup] = useState(false);
+    const [showEditPopup, setShowEditPopup] = useState(false);
 
 //Bei Änderung von id ausführen
     useEffect(()=> {
@@ -56,7 +57,7 @@ export default function Learning(){
         
     }
     
-//Bestätigung im Pop-up-Fenster fürs Löschen
+//Bestätigung im Popup-Fenster fürs Löschen
     function handleDeleteCard() {
         setShowPopup(true);
     }
@@ -77,21 +78,44 @@ export default function Learning(){
             karten:copycurrentCards
         };
 
+
+        //falls man die letzte Karte gelöscht hat, index anpassen 
+        if (copycurrentCards.length ===0) {
+            setIndex(0); //greift oben auf "dieses  Set hat noch keine karten"
+        } else if(index >=copycurrentCards.length){
+            setIndex(copycurrentCards.length-1);
+        }
+
         await db.lernsets.put(newcurrentSet);
         setCurrentSet(newcurrentSet);
         setShowPopup(false);
 
-        //falls man die letzte Karte gelöscht hat, index anpassen 
-        if (index >= copycurrentCards.length && index > 0) {
-            setIndex(index - 1);
-        }
+
+    }
+
+    async function handleUpdateCard(neueFrage, neueAntwort){
+        const copycurrentCards=[...currentSet.karten];
+        copycurrentCards[index] = {
+            ...copycurrentCards[index],
+            frage: neueFrage,
+            antwort: neueAntwort
+        };
+
+        const newcurrentSet = {
+            ...currentSet,
+            karten: copycurrentCards
+        };
+
+        await db.lernsets.put(newcurrentSet);
+        setCurrentSet(newcurrentSet);
+        setShowEditPopup(false);
     }
 
     return (
     <>
 {/* Steuerungsbuttons oben rechts*/}
         <div className="top-buttons">
-            <button className="edit-button">✏️Bearbeiten</button>
+            <button className="edit-button" onClick={()=> setShowEditPopup(true)}>✏️Bearbeiten</button>
             <button className="delete-button" onClick={handleDeleteCard}>🗑️Löschen</button>
         </div>
 
@@ -114,21 +138,28 @@ export default function Learning(){
         </div>
 
 
-{/*Pop-up-Fenster bauen */}
-        {showPopup && (
-            <div className="popup-background">
-                <div className="popup-box">
-                    <h3>Bist du sicher?</h3>
-                    <p>Möchtest du diese Lernkarte wirklich löschen?</p>
-                    <div className="popup-buttons">
-                        <button className="popup-cancel" onClick={handleCancelDelete}>Abbrechen</button>
-                        <button className="popup-confirm" onClick={handleConfirmDelete}>Löschen</button>
-                    </div>
+{/*Popup-Fenster bauen fürs löschen*/}
+    {showPopup && (
+        <div className="popup-background">
+            <div className="popup-box">
+                <h3>Bist du sicher?</h3>
+                <p>Möchtest du diese Lernkarte wirklich löschen?</p>
+                <div className="popup-buttons">
+                    <button className="popup-cancel" onClick={handleCancelDelete}>Abbrechen</button>
+                    <button className="popup-confirm" onClick={handleConfirmDelete}>Löschen</button>
                 </div>
             </div>
-        )}
+        </div>
+    )}
 
-
+{/*edit Popup-Fenster anzeigen */}
+    {showEditPopup &&(
+        <Edit 
+        card={currentSet.karten[index]} 
+        onClose={() => setShowEditPopup(false)} 
+        onSave={handleUpdateCard}
+        />
+    )}
 
     </>
     );
